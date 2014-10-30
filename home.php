e2f8478e7021e1b46306307847beb7617c6d4046
mattfairley <matt.fairley@gmail.com>
2014-10-30 14:22:26 -0400

    Some more stuff - footer etc

--- home.php
+++ home.php
@@ -2,8 +2,15 @@
 <?php get_header(); ?>
 
 <!-- carousel -->
-
+<div class="carousel"></div>
 <!-- general info -->
+<div class="info">
+	<div class="container">
+		<img src="http://placecage.com/g/200/200" alt="" class="icon">
+		<p class="info"><span class="event-name"><?php bloginfo( 'name' ); ?></span> is a 1-4 day festival or event taking place <span class="event-date">November 17-18</span> in your local area.</p>
+	</div>
+</div>
+
 
 <div class="main">
   <div class="container">
